e885edb7244b3f8affc5524ebd12f91a8913f68f
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-02-25 19:18:13 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const SWITCH_COLOR_DELAY = 1000;
+let intervalId = null;
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', handleStartBtnClick);
+refs.stopBtn.addEventListener('click', handleStopBtnClick);
+
+function handleStartBtnClick() {
+  setRandomBodyBgColor();
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    setRandomBodyBgColor();
+  }, SWITCH_COLOR_DELAY);
+}
+
+function handleStopBtnClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setRandomBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
